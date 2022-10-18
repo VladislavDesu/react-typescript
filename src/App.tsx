9dd1bc8ec058b2f336200b764b35badfd6bc4eda
Vladislav Gryadko <vgryadko@gmail.com>
2022-10-18 18:41:00 +0300

    new version

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "assets/styles/global.scss";
+
+const App: React.FC = () => {
+    return (
+        <div className="App">Hello, World!</div>
+    );
+};
+
+export default App;
